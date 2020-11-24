f86fec8d3fcefef92b1cd0de756500b958a4effe
= <t8190130@aueb.gr>
2020-11-24 16:53:59 +0200

    prosuesa kapoiesc epiloges sto menu epilogis

--- AirlineDog/Url1.java
+++ AirlineDog/Url1.java
@@ -7,5 +7,7 @@ import java.util.logging.Logger;
 //geografika diamerismata 
 public class Url1 {
 //menou epiogis
-	//
+	/System.out.println("*********** ΤΑ ΣΤΑΤΙΣΤΙΚΑ ΣΤΟΙΧΕΙΑ ΣΧΕΤΙΚΑ ΜΕ ΤΑ ΓΕΩΓΡΑΦΙΚΑ ΔΙΑΜΕΡΙΣΜΑΤΑ ΤΗΣ ΕΛΛΑΔΑΣ  ************");
+	System.out.println("1.Γεωγραφικό Διαμέρισμα Αττική ");
+	System.out.println("2.Γεωγραφικό Διαμέρισμα Ήπειρος ");
 }
